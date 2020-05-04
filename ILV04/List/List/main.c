a27125fdbb0c11878135241ecdb9f2c235336432
GRiSDOF <christoph_lenzbauer@yahoo.de>
2020-05-04 23:40:55 +0200

    Pushed ILV04

---
+++ ILV04/List/List/main.c
@@ -0,0 +1,28 @@
+//////////////////////////////////////////////////////////////////
+//		  Salzburg University of Applied Sciences				//
+//		Information Technology & Systems Management				//
+//						SWE2-ILV/B								//
+//////////////////////////////////////////////////////////////////
+// date:			04.05.2020									//
+//																//
+//																//
+// author:			Christoph Lenzbauer							//
+// email:			clenzbauer.its-b2019@fh-salzburg.ac.at		//
+//////////////////////////////////////////////////////////////////
+
+
+#include <stdio.h>
+#include <conio.h>
+#include <locale.h>
+#include <stdbool.h>
+#include <string.h>
+#include "list.h"
+#include "list.c"
+
+#pragma warning(disable:4996)
+
+
+void main()
+{
+	//Insert specialized Sourcecode for special application here
+}
